0923181e276273d1fbe76fa1514668521bb8d222
JJH <junho7339@naver.com>
2019-11-26 10:11:59 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 진준호 on 2019/11/26.
+//  Copyright © 2019 진준호. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
